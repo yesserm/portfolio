4cb2ddc54de8115fac27f410ad592a1a75df4244
yesserm <yessermiranda0106@gmail.com>
2026-06-23 14:17:40 -0600

    Inital structure to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind"
 
-// https://astro.build/config
-export default defineConfig({});
+export default {
+    integrations: [tailwind()],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
